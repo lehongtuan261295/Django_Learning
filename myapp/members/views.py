76767e326af12b584be55f3239c4792ae4e0372a
lehongtuan261295 <lehongtuan261295@gmail.com>
2023-07-20 17:37:01 +0700

    update template.html

--- myapp/members/views.py
+++ myapp/members/views.py
@@ -27,8 +27,9 @@ def main(request):
 
 
 def testing(request):
+    mymembers = Member.objects.all().values()
     template = loader.get_template('template.html')
     context = {
-        'fruits': ['Apple', 'Banana', 'Cherry'],
+        'mymembers': mymembers,
     }
     return HttpResponse(template.render(context, request))
